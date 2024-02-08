7a7e16273873eae5263e2bc1e8d5eb8356fce700
JossAbreu <josue.abreu@hytechgizmo.com>
2024-02-08 14:54:42 -0400

    update

--- src/components/layouts/Home.vue
+++ src/components/layouts/Home.vue
@@ -5,7 +5,7 @@ import content from '../organisms/content.vue'
 </script>
 
 <template>
- <section class="h-full w-full shadow-md bg-background grid grid-rows-[auto,1fr] rounded-3xl shadow-black ring-[2.2px] ring-borders">
+ <section class="h-full w-full shadow-md bg-background grid grid-rows-[auto,1fr] rounded-3xl shadow-black ring-[2.2px] ring-black">
      <Nav />
      <content />
     
